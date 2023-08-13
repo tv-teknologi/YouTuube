1279eb8e74642cd9a214730e43b9407ae9063816
tv-teknologi <teknologitv7@mail.com>
2023-08-13 11:56:39 +0700

    catatan github

---
+++ github_.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?= "WELCOME TO TV TEKNOLOGI !"; ?>
+    <h1>Hello World!</h1>
+    <p>
+        git config --global user.email emailgithub@mail.com
+        git config --global user.name usernamegithub
+
+        #masuk direktori folder
+        git init
+        git remote add origin url_github
+        git commit -m "komentar commitnya"
+        git branch -m main
+        git push origin main
+
+        #semisal belum bisa bersihkan dulu
+        git branch --uset-upstream
+
+    </p>
+</body>
+</html>
